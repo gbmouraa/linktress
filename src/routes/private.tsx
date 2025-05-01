964f150b5292d87137e89c8529114ca40d940cb3
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-04-30 22:36:03 -0300

    feat: add Private route component for protected dashboard access

---
+++ src/routes/private.tsx
@@ -0,0 +1,45 @@
+import { useState, ReactNode, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "@/services/firebase-connection";
+import { onAuthStateChanged } from "firebase/auth";
+
+interface PrivateProps {
+  children: ReactNode;
+}
+
+export const Private = ({ children }: PrivateProps) => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsSignedIn(true);
+      } else {
+        setIsSignedIn(false);
+      }
+    });
+    setIsLoading(false);
+
+    return () => unsub();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen w-full items-center bg-white">
+        <ul className="spinner">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+
+  if (!isSignedIn) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return children;
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Login } from "../pages/login";
 import { Register } from "../pages/register";
+import { Private } from "./private";
+import { Dashboard } from "../pages/dashboard";
 
 export const AppRoutes = () => {
   return (
@@ -10,6 +12,14 @@ export const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Private>
+              <Dashboard />
+            </Private>
+          }
+        />
       </Routes>
     </div>
   );
