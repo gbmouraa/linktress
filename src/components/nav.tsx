3486a0289d9809d637a9e4f1247066023c51bbcb
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-05-06 21:43:08 -0300

    feat: enhance Nav component with dynamic path highlighting and improved layout

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,37 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import homeIcon from "@/assets/menu-icons/home_menu_icon.svg";
 import customizeIcon from "@/assets/menu-icons/customize_page_menu.svg";
 import pencilIcon from "@/assets/menu-icons/edit_sdwc_menu.svg";
 
 export const Nav = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
-    <nav className="hidden h-screen w-[300px] border-r-2 p-8 md:block">
-      <ul className="flex flex-col space-y-4">
-        <li className="rounded-full transition-colors hover:bg-zinc-100">
+    <nav className="p-8 md:h-screen md:w-[300px] md:border-r-2">
+      <ul className="flex items-center justify-evenly gap-4 md:flex-col md:items-start md:justify-start">
+        <li
+          className={`rounded-full transition-colors md:hover:bg-zinc-100 ${currentPath === "/admin" && "md:bg-zinc-100"}`}
+        >
           <Link
             to="/admin"
-            className="flex h-full w-full items-center gap-x-2 py-3 pl-8 text-sm"
+            className="flex h-full flex-col items-center justify-center gap-x-2 text-xs md:min-w-[236px] md:flex-row md:justify-start md:py-3 md:pl-8 md:text-sm"
           >
-            <img src={homeIcon} alt="home icon" />
-            Home
+            <div
+              className={`rounded-full bg-zinc-100 p-4 md:rounded-none md:bg-transparent md:p-0 ${currentPath === "/admin" && "bg-indigo-600/20"}`}
+            >
+              <img src={homeIcon} alt="home icon" />
+            </div>
+            <span className="mt-6 -translate-y-[3px] md:mt-0 md:-translate-y-0">
+              Home
+            </span>
           </Link>
         </li>
-        <li className="rounded-full transition-colors hover:bg-zinc-100">
+        <li
+          className={`rounded-full transition-colors md:hover:bg-zinc-100 ${currentPath === "/admin/edit" && "md:bg-zinc-100"}`}
+        >
           <Link
             to="/admin/edit"
-            className="flex h-full w-full items-center gap-x-2 py-3 pl-8 text-sm"
+            className="flex w-full flex-col items-center justify-center gap-x-2 text-xs md:min-w-[236px] md:flex-row md:justify-start md:py-3 md:pl-8 md:text-sm"
           >
-            <img src={pencilIcon} alt="pencil icon" />
-            Editar linktress
+            <div
+              className={`rounded-full bg-zinc-100 p-4 md:rounded-none md:bg-transparent md:p-0 ${currentPath === "/admin/edit" && "bg-indigo-600/20"}`}
+            >
+              <img src={pencilIcon} alt="pencil icon" />
+            </div>
+            <span className="mt-6 md:mt-0">Editar</span>
           </Link>
         </li>
-        <li className="rounded-full transition-colors hover:bg-zinc-100">
+        <li
+          className={`rounded-full transition-colors md:hover:bg-zinc-100 ${currentPath === "/admin/customize" && "md:bg-zinc-100"}`}
+        >
           <Link
             to="/admin/customize"
-            className="flex h-full w-full items-center gap-x-2 py-3 pl-8 text-sm"
+            className="flex w-full flex-col items-center justify-center gap-x-2 text-xs md:min-w-[236px] md:flex-row md:justify-start md:py-3 md:pl-8 md:text-sm"
           >
-            <img src={customizeIcon} alt="customize icon" />
-            Customizar página
+            <div
+              className={`rounded-full bg-zinc-100 p-4 md:rounded-none md:bg-transparent md:p-0 ${currentPath === "/admin/customize" && "bg-indigo-600/20"}`}
+            >
+              <img src={customizeIcon} alt="customize icon" />
+            </div>
+            <span className="mt-6 md:mt-0">Customizar</span>
           </Link>
         </li>
       </ul>
