661cb59c117e73508a760b40084e56037d8f8963
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-05-08 23:13:50 -0300

    feat: implement user profile loading with dynamic background and alert dialog

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,49 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { getUserProfile, UserDataType } from "../utils/firebase";
+import { PiLinktreeLogoLight } from "react-icons/pi";
+import { AlertDialogProfilePage } from "../components/alert-dialog-profile-page";
 
 export const Profile = () => {
-  const { profile } = useParams();
+  const [userData, setUserData] = useState<UserDataType | null>();
 
-  return <div>{profile}</div>;
+  const { profile } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await getUserProfile(profile!);
+      if (data) {
+        setUserData(data);
+      } else {
+        navigate("/");
+      }
+    };
+
+    loadData();
+  }, [profile, navigate]);
+
+  return (
+    <div
+      className="h-screen w-full pt-5"
+      style={{ backgroundColor: userData?.pageBg || "#ffffff" }}
+    >
+      <section
+        aria-label="Página do usuário"
+        className="mx-auto w-full max-w-[580px] px-4"
+      >
+        <header>
+          <div className="flex items-center justify-between">
+            <Link to="/">
+              <PiLinktreeLogoLight color="#fff" size={40} />
+            </Link>
+            <AlertDialogProfilePage
+              username={userData?.username}
+              profileURL="https://linktress/profile/gabriel"
+            />
+          </div>
+        </header>
+      </section>
+    </div>
+  );
 };
